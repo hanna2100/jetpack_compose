c32a1904b1b14c759994b06d0833a5909531bd3e
hanna2100 <hanna0497@naver.com>
2020-10-18 18:07:32 +0900

    layout and image

--- app/src/main/java/com/example/jetpack/compose/MainActivity.kt
+++ app/src/main/java/com/example/jetpack/compose/MainActivity.kt
@@ -2,21 +2,69 @@ package com.example.jetpack.compose
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.setContent
+import androidx.compose.ui.res.imageResource
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
 import androidx.ui.tooling.preview.Preview
+import com.example.test.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-
+            NewsStory()
         }
     }
 }
+@Composable
+fun Greeting(name: String) {
+    Text (text = "Hello $name!")
+}
+@Composable
+fun NewsStory() {
+    val image = imageResource(id = R.drawable.header)
+    MaterialTheme() {
+        val typograpy = MaterialTheme.typography
+        Column(
+            modifier = Modifier.padding(16.dp)
+        ) {
+            val imageModifier = Modifier
+                .preferredHeight(180.dp)
+                .fillMaxWidth()
+                .clip(shape = RoundedCornerShape(4.dp))
+
+            Image(image,
+                modifier = imageModifier,
+                contentScale = ContentScale.Crop
+            )
+            Spacer(modifier = Modifier.preferredHeight(16.dp))
+            Text("A day wandering through the sandhills " +
+                    "in Shark Fin Cove, and a few of the " +
+                    "sights I saw",
+                style = typograpy.h6,
+                maxLines = 2,
+                overflow = TextOverflow.Ellipsis)
+            Text("Davenport, California",
+                style = typograpy.body2)
+            Text("December 2018",
+                style = typograpy.body2)
+        }
+    }
+}
+
 @Preview
 @Composable
-fun PreviewGreeting() {
-    Text("hello~!")
+fun DefaultPreview() {
+    NewsStory()
 }
+
