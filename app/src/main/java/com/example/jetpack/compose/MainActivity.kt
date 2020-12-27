0285d7cd80569d1304e15bec4d36c4e2685558ac
hanna2100 <hanna0497@naver.com>
2020-12-27 17:46:14 +0900

    mapper entity

--- app/src/main/java/com/example/jetpack/compose/MainActivity.kt
+++ app/src/main/java/com/example/jetpack/compose/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.jetpack.compose
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.jetpack.compose.domain.model.Recipe
+import com.example.jetpack.compose.network.model.RecipeNetworkEntity
+import com.example.jetpack.compose.network.model.RecipeNetworkMapper
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val mapper = RecipeNetworkMapper()
+        val recipe = Recipe()
+        val networkEntity: RecipeNetworkEntity = mapper.mapToEntity(recipe)
+
+        val r = mapper.mapFromEntity(networkEntity)
+        
     }
 }
